4cb74276449faf64e07b08c2790429108a584b30
Dylan Davidson <ddavidson621@gmail.com>
2015-02-26 02:26:10 -0500

    Initial Commit.

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomManager : MonoBehaviour {
+
+	public GameObject blankFloor;
+	public GameObject wall;
+	// Use this for initialization
+	void Start () {
+		TextAsset room1 = (TextAsset) Resources.Load ("Room1");
+		float y = 4;
+		float x = -8;
+		Random rand = new Random();
+		for(int i = 0; i < room1.text.Length; i++) {
+			if(room1.text[i] == 'X') {
+				Instantiate(wall, new Vector3(x, y, 1), Quaternion.identity);
+				x += 1;
+			}
+			else if(room1.text[i] == 'O') {
+				Instantiate(blankFloor, new Vector3(x, y, 1), Quaternion.identity);
+				x += 1;
+			}
+			else if(room1.text[i] == 'F') {
+				if(Random.value < .5)
+					Instantiate(wall, new Vector3(x, y, 1), Quaternion.identity);
+				else
+					Instantiate(blankFloor, new Vector3(x, y, 1), Quaternion.identity);
+				x += 1;
+			}
+			if(x > 8) {
+				x = -8;
+				y -= 1;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Animator animator;
+	public float speed = 5f;
+	bool moved;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		animator.SetBool ("moving", false);
+		int att = animator.GetInteger ("attacking");
+		animator.SetInteger("direction", 0);
+		if (att > 0)
+						animator.SetInteger ("attacking", att - 1);
+				else {
+						if (Input.GetKey (KeyCode.Space)) {
+								if (animator.GetInteger ("attacking") == 0)
+										animator.SetInteger ("attacking", 15);
+						}
+						if (Input.GetKey (KeyCode.LeftArrow)) {
+								animator.SetBool ("moving", true);
+								animator.SetInteger ("direction", 4);
+								Vector3 position = this.transform.position;
+								position.x -= speed * Time.deltaTime;
+								this.transform.position = position;
+								moved = true;
+						}
+						if (Input.GetKey (KeyCode.RightArrow)) {
+								animator.SetBool ("moving", true);
+								animator.SetInteger ("direction", 2);
+								Vector3 position = this.transform.position;
+								position.x += speed * Time.deltaTime;
+								this.transform.position = position;
+								moved = true;
+						}
+						if (Input.GetKey (KeyCode.UpArrow)) {
+								animator.SetBool ("moving", true);
+								animator.SetInteger ("direction", 3);
+								Vector3 position = this.transform.position;
+								position.y += speed * Time.deltaTime;
+								this.transform.position = position;
+								moved = true;
+						}
+						if (Input.GetKey (KeyCode.DownArrow)) {
+								animator.SetBool ("moving", true);
+								Vector3 position = this.transform.position;
+								position.y -= speed * Time.deltaTime;
+								this.transform.position = position;
+								animator.SetInteger ("direction", 1);
+								moved = true;
+						}
+				}
+	}
+}
