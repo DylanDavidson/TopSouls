d2aafef6f8264bda6d6431a680b2a61704984ab6
Dylan Davidson <ddavidson621@gmail.com>
2015-02-26 23:33:38 -0500

    Gets basic camera movement between rooms working.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private int leftx = -8;
+	private int rightx = 8;
+	private int upy = 4;
+	private int downy = -4;
+	private Transform playerTransform;
+
+	void Start() {
+		playerTransform = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerTransform.position.x < leftx) {
+			leftx -= 16;
+			rightx -= 16;
+			Vector3 cam = transform.position;
+			cam.x -= 17;
+			transform.position = cam;
+		}
+		else if(playerTransform.position.x > rightx) {
+			leftx += 16;
+			rightx += 16;
+			Vector3 cam = transform.position;
+			cam.x += 17;
+			transform.position = cam;
+		}
+
+	}
+}
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -6,11 +6,10 @@ public class RoomManager : MonoBehaviour {
 	public GameObject blankFloor;
 	public GameObject wall;
 	// Use this for initialization
-	void Start () {
+	void Room1() {
 		TextAsset room1 = (TextAsset) Resources.Load ("Room1");
 		float y = 4;
 		float x = -8;
-		Random rand = new Random();
 		for(int i = 0; i < room1.text.Length; i++) {
 			if(room1.text[i] == 'X') {
 				Instantiate(wall, new Vector3(x, y, 1), Quaternion.identity);
@@ -32,5 +31,37 @@ public class RoomManager : MonoBehaviour {
 				y -= 1;
 			}
 		}
+	}
+
+	void Room2() {
+		TextAsset room2 = (TextAsset) Resources.Load ("Room2");
+		float y = 4;
+		float x = -25;
+		for(int i = 0; i < room2.text.Length; i++) {
+			if(room2.text[i] == 'X') {
+				Instantiate(wall, new Vector3(x, y, 1), Quaternion.identity);
+				x += 1;
+			}
+			else if(room2.text[i] == 'O') {
+				Instantiate(blankFloor, new Vector3(x, y, 1), Quaternion.identity);
+				x += 1;
+			}
+			else if(room2.text[i] == 'F') {
+				if(Random.value < .5)
+					Instantiate(wall, new Vector3(x, y, 1), Quaternion.identity);
+				else
+					Instantiate(blankFloor, new Vector3(x, y, 1), Quaternion.identity);
+				x += 1;
+			}
+			if(x >= -8) {
+				x = -25;
+				y -= 1;
+			}
+		}
+	}
+
+	void Start () {
+		Room1 ();
+		Room2 ();
 	}
 }
