8258d6d288ea531bfafe6b4d9f71f06e0a935b5d
Bioninja01 <vinsonjames02@gmail.com>
2015-03-04 15:49:32 -0500

    made the algroithm more clear

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 
 public class MapGeneration : MonoBehaviour {
-
+	int x2;
+	int y2;
 	public GameObject prefab;
 	public int row;
 	public int col;
@@ -21,7 +22,7 @@ public class MapGeneration : MonoBehaviour {
 		}
 		int x = 0;
 		int y = Random.Range (0,col);
-		grid [x, y] = 1;
+		grid [x, y] = -1;
 		CreatMap (x,y);
 		for (int i=0; i<row; i++) {
 			for(int j=0; j<col;j++)
@@ -43,7 +44,16 @@ public class MapGeneration : MonoBehaviour {
 						SpriteRenderer sr = map[i,j].GetComponent<SpriteRenderer>();
 						sr.color = Color.blue;
 					}
-
+					if(grid[i,j]==4){
+						map[i,j] = (GameObject)Instantiate(prefab,new Vector3(i*.32f,j*.32f,0),Quaternion.identity);
+						SpriteRenderer sr = map[i,j].GetComponent<SpriteRenderer>();
+						sr.color = Color.gray;
+					}
+					if(grid[i,j]==-1){
+						map[i,j] = (GameObject)Instantiate(prefab,new Vector3(i*.32f,j*.32f,0),Quaternion.identity);
+						SpriteRenderer sr = map[i,j].GetComponent<SpriteRenderer>();
+						sr.color = Color.cyan;
+					}
 					else{
 						map[i,j] = (GameObject)Instantiate(prefab,new Vector3(i*.32f,j*.32f,0),Quaternion.identity);
 					}
@@ -96,6 +106,7 @@ public class MapGeneration : MonoBehaviour {
 					else if(grid[test,y]!=0){
 						continue;
 					}
+					grid[x,y] =4;
 					x=x+1;
 					grid[x,y]=3;
 					break;
@@ -124,4 +135,46 @@ public class MapGeneration : MonoBehaviour {
 	}
 
 
+	/*void CreatMap2(int x, int y){
+		bool finished = false;
+		while(!finished){
+			int [] choise = {1,2,3,1,2,1,2};
+			shuffleArray (choise);
+			int test;
+			for(int i=0;i<choise.Length;i++){
+				switch(choise[i]){
+				case 1:
+					test = y+1;
+					if(test>=col || grid[x,test]!=0){
+						continue;
+					}
+					y=y+1;
+					grid[x,y]=1;
+					break;
+				case 2:
+					test = y-1;
+					if(test<0 || grid[x,test]!=0){
+						continue;
+					}
+					y=y-1;
+					grid[x,y]=2;
+					break;
+				case 3:
+					test = x-1;
+					if(test<0){
+						finished = true;
+						grid[x,y]=5;
+						return;
+					}
+					else if(grid[test,y]!=0){
+						continue;
+					}
+					x=x-1;
+					grid[x,y]=3;
+					break;
+				}
+			}
+		}
+		
+	}*/
 }
