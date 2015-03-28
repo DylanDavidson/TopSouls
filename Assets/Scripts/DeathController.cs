fe81b7649efe9b99df2c679f031deb5e3196c755
TrevorRoman <troman360@gmail.com>
2015-03-28 15:04:29 -0400

    Death scene transition and player taunting added

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathController : MonoBehaviour {
+
+	IEnumerator Restart()
+	{
+		yield return new WaitForSeconds (3);
+		if(Input.anyKey)
+			Application.LoadLevel ("final_scene1");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		StartCoroutine(Restart ());
+	}
+}
