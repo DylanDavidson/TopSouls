676c1d324e7ce48f680f35d55ba84218b4fb4a9a
Bioninja01 <vinsonjames02@gmail.com>
2015-04-07 22:56:51 -0400

    small changes

--- Assets/Scripts/JamesTeatScripts/Room0_Gen.cs
+++ Assets/Scripts/JamesTeatScripts/Room0_Gen.cs
@@ -5,6 +5,11 @@ public class Room0_Gen : MonoBehaviour {
 	public GameObject floor;
 	public GameObject wall;
 	public GameObject door;
+
+	private int num_floor = 0;
+	private int num_door = 2;
+	private int num_wall = 1;
+
 	private int row;
 	private int col;
 
@@ -16,6 +21,7 @@ public class Room0_Gen : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		Dice d = Dice.getInatance ();
 		NumGen ng = NumGen.getInatance (); 
 		row = ng.getX ();
 		col = ng.getY ();
@@ -24,7 +30,48 @@ public class Room0_Gen : MonoBehaviour {
 		grid = new int[row, col];
 		for (int i=0; i<row; i++) {
 			for(int j=0; j<col;j++){
-				grid[i,j] = 0;
+				if((i ==(int)row/2)&&(j==0||j==col-1)){
+					d.roll();
+					if(d.getVal() < d.getMaxVal()){
+						grid[i,j] =num_door;
+					}
+					else{grid[i,j] =num_wall;}
+				}
+				else if((j ==(int)col/2)&&(i==row-1 || i ==0)){
+					d.roll();
+					if(d.getVal() < d.getMaxVal()/4){
+						grid[i,j] =num_door;
+					}
+					else{grid[i,j] =num_wall;}
+				}
+				else if( (i==0 || i == row-1) ){
+					grid[i,j]=num_wall;
+				}
+				else if( (j==0 || j == col-1) ){
+					grid[i,j]=num_wall;
+				}
+				else{
+					if(row > 5 && col > 5 && i>1 && i<row-2 
+					   && j>1 && j<col-2){
+
+						d.roll();
+						if(d.getVal() < d.getMaxVal()){
+							grid[i,j] = num_floor;
+						}
+						else{
+							grid[i,j] = num_wall;
+						}
+					}
+					else{
+						grid[i,j]=num_floor;
+					}
+				}
+
+
+
+
+
+				//grid[i,j] = 0;
 			}
 		}
 		
@@ -33,8 +80,19 @@ public class Room0_Gen : MonoBehaviour {
 			{
 				float position_x = transform.position.x+i*tileSize;
 				float position_y = transform.position.y+j*tileSize;
-				if(grid[i,j]==0){
-					map[i,j] = (GameObject)Instantiate(wall,new Vector3(position_x,position_y,0),Quaternion.identity);
+				if(grid[i,j] !=0)
+				{
+					if(grid[i,j]==num_wall){
+						map[i,j] = (GameObject)Instantiate(wall,new Vector3(position_x,position_y,0),Quaternion.identity);
+						map[i,j].transform.parent = transform;
+					}
+					if(grid[i,j]==num_door){
+						map[i,j] = (GameObject)Instantiate(door,new Vector3(position_x,position_y,0),Quaternion.identity);
+						map[i,j].transform.parent = transform;
+					}
+				}
+				if(grid[i,j]==num_floor){
+					map[i,j] = (GameObject)Instantiate(floor,new Vector3(position_x,position_y,0),Quaternion.identity);
 					map[i,j].transform.parent = transform;
 				}
 			}
