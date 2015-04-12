4e52c1bdff4065bc9c17d75a6143e329af3147cb
TrevorRoman <troman360@gmail.com>
2015-04-12 00:08:52 -0400

    Player health/stamina sliders

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 public class UIController : MonoBehaviour {
 	public Text health;
 	public Text stamina;
+	public Slider healthSlider;
+	public Slider staminaSlider;
 	public PlayerController playerController;
 
 	// Use this for initialization
@@ -13,13 +15,19 @@ public class UIController : MonoBehaviour {
 		playerController = GameObject.Find ("Player").GetComponent<PlayerController> ();
 		health = GameObject.Find ("Health").GetComponent<Text> ();
 		stamina = GameObject.Find ("Stamina").GetComponent<Text> ();
+		healthSlider = GameObject.Find ("HealthSlider").GetComponent<Slider> ();
+		staminaSlider = GameObject.Find ("StaminaSlider").GetComponent<Slider> ();
 		health.text = "HEALTH: " + playerController.health;
 		stamina.text = "STAMINA: " + playerController.stamina;
+		healthSlider.value = playerController.health;
+		staminaSlider.value = playerController.stamina;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		health.text = "HEALTH: " + playerController.health;
 		stamina.text = "STAMINA: " + playerController.stamina;
+		healthSlider.value = playerController.health;
+		staminaSlider.value = playerController.stamina;
 	}
 }
