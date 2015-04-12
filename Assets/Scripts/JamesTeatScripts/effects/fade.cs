41e214566e54dd197c2a9afe2ce62be5b2b8d0c5
Bioninja01 <vinsonjames02@gmail.com>
2015-04-12 02:39:42 -0400

    added a fade effect to the death scene

---
+++ Assets/Scripts/JamesTeatScripts/effects/fade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fade : MonoBehaviour {
+	public Image damageImage;  
+	public float flashSpeed;
+	public Color flashColour = Color.black; 
+
+	void Start(){
+		damageImage.color = flashColour;
+	}
+	// Update is called once per frame
+	void Update () {
+		damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+	}
+}
