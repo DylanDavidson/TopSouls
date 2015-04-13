dede6a7e68bc3b7d640082602c7bc960112d4a04
Dylan Davidson <ddavidson621@gmail.com>
2015-04-13 14:09:07 -0400

    WIP.

--- Assets/Scripts/JamesTeatScripts/classes/EnemySpawner.cs
+++ Assets/Scripts/JamesTeatScripts/classes/EnemySpawner.cs
@@ -24,6 +24,7 @@ public class EnemySpawner
 				new Vector3(spawn.transform.position.x, spawn.transform.position.y, -1), 
 				Quaternion.identity
 			);
+			temp.layer = 1;
 			temp.transform.parent = spawn.transform;
 			enemies.Add(temp.GetComponent<EnemyPlaceholderController>());
 			roomDifficulty -= 5;
