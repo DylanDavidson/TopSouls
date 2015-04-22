fccda81fa9d94dc165d5b8a50bbd4b1284168c4a
Dylan Davidson <ddavidson621@gmail.com>
2015-04-22 16:18:51 -0400

    Random fixes dear god.

--- Assets/Scripts/JamesTeatScripts/classes/NumGen.cs
+++ Assets/Scripts/JamesTeatScripts/classes/NumGen.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class NumGen{
 	public static NumGen instance = new NumGen();
-	public static int x = Random.Range(3,32);
-	public static int y = Random.Range(3,32);
+	public static int x = Random.Range(6,32);
+	public static int y = Random.Range(6,32);
 	
 
 	public static NumGen getInatance(){return instance;}
